945dc81893243caba619a480cdf3700a006e990e
desamesa <delgadomesa@gmail.com>
2023-11-16 19:50:23 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+let contenidoTitulo = "John Delgado";
+
+
+//CONDICIONALES
+
+let textoTitulo = titulo.innerText;
+console.log(textoTitulo);
+
+
+if(textoTitulo == "John Delgado"){
+    titulo.innerHTML = "Nombre";
+
+}
+
+else {
+    console.log("no se cumple");
+}
